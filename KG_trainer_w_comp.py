94c5e7e23a609aedb575422c69757ee0d4cfda83
estherch0 <126043648+estherch0@users.noreply.github.com>
2025-03-09 21:43:26 -0700

    Update KG_trainer_w_comp.py

--- KG_trainer_w_comp.py
+++ KG_trainer_w_comp.py
@@ -340,7 +340,7 @@ def get_KG_RGCN_trainer(
         overwrite_output_dir=True,
         num_train_epochs=epochs,
         per_device_train_batch_size=train_batch_size,
-        fp16=True,
+        fp16=False,
         save_steps=5000,
         save_total_limit=3,
         logging_steps=500,
